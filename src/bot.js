9271af27c0b049d65efe92dfb28d4249086d2b3f
Rudra Patel <patelrudra3086@gmail.com>
2025-07-05 21:48:34 +0530

    initial commit

---
+++ src/bot.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+const PREFIX="$";
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+   if (message.content === 'Hello') {
+    message.channel.send('Hey Buddy');
+  }
+  if(message.content.startsWith(PREFIX)){
+    const [cmd_name,...args]=message.content
+    .trim()
+    .substring(PREFIX.length)
+    .split(" ");
+    if(cmd_name==='kick'){
+      if(message.member)
+      if(args.length===0){return message.reply("please provide an Id")}
+      // message.channel.send(`${args} kicked off the server`);
+      const member=message.guild.members.cache.get(args[0]);
+      if(member){
+        member.kick()
+        .catch((err)=> message.channel.send(`i do not have permissions to kick ${member}`))
+        .then((message.channel.send(`${member} was kicked.`)));
+        
+      }else{
+        message.channel.send("The member not found");
+      }
+  
+    }
+  }
+  // else if (message.content === 'Hello') {
+  //   message.send('Hey Buddy');
+  // }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
